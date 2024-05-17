73aa8db3c3698f44f9ac702da68831a79be5d75d
Youssef Moustafa <youssef.moustafa2012amer@gmail.com>
2024-05-17 17:59:25 +0200

    el sho8l el mazboot

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -16,8 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
-            Application.Run(new AdminForm());
+            Application.Run(new Customer());
         }
     }
 }
