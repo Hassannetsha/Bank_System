841d872e16e43251c2715db7aff6e87040dcb2c0
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2024-05-18 20:54:53 +0300

    draft

--- WindowsFormsApp1/WindowsFormsApp1/Request Loan.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Request Loan.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,6 +40,21 @@
             this.label5 = new System.Windows.Forms.Label();
             this.text_OriginationDate = new System.Windows.Forms.TextBox();
             this.btn_Request = new System.Windows.Forms.Button();
+            this.txt_AccountId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bankSystemDataSet = new WindowsFormsApp1.BankSystemDataSet();
+            this.aCCOUNTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aCCOUNTTableAdapter = new WindowsFormsApp1.BankSystemDataSetTableAdapters.ACCOUNTTableAdapter();
+            this.aCCOUNTIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cUSTOMERIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bALANCEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tYPEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iNTERESTRATEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.oPENDATEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bankSystemDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aCCOUNTBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
@@ -62,9 +78,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(355, 19);
+            this.label1.Location = new System.Drawing.Point(639, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 41);
+            this.label1.Size = new System.Drawing.Size(151, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "Branch Loans";
             // 
@@ -72,7 +88,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(543, 151);
+            this.label2.Location = new System.Drawing.Point(543, 140);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 18);
             this.label2.TabIndex = 2;
@@ -82,7 +98,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(543, 101);
+            this.label3.Location = new System.Drawing.Point(543, 90);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 18);
             this.label3.TabIndex = 3;
@@ -90,14 +106,14 @@
             // 
             // text_LoanType
             // 
-            this.text_LoanType.Location = new System.Drawing.Point(646, 98);
+            this.text_LoanType.Location = new System.Drawing.Point(646, 87);
             this.text_LoanType.Name = "text_LoanType";
             this.text_LoanType.Size = new System.Drawing.Size(192, 22);
             this.text_LoanType.TabIndex = 4;
             // 
             // text_Interestrate
             // 
-            this.text_Interestrate.Location = new System.Drawing.Point(646, 147);
+            this.text_Interestrate.Location = new System.Drawing.Point(646, 136);
             this.text_Interestrate.Name = "text_Interestrate";
             this.text_Interestrate.Size = new System.Drawing.Size(192, 22);
             this.text_Interestrate.TabIndex = 5;
@@ -123,7 +139,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(506, 228);
+            this.label5.Location = new System.Drawing.Point(510, 225);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(122, 18);
             this.label5.TabIndex = 8;
@@ -131,7 +147,7 @@
             // 
             // text_OriginationDate
             // 
-            this.text_OriginationDate.Location = new System.Drawing.Point(646, 225);
+            this.text_OriginationDate.Location = new System.Drawing.Point(646, 221);
             this.text_OriginationDate.Name = "text_OriginationDate";
             this.text_OriginationDate.Size = new System.Drawing.Size(192, 22);
             this.text_OriginationDate.TabIndex = 9;
@@ -142,18 +158,120 @@
             this.btn_Request.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btn_Request.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Request.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btn_Request.Location = new System.Drawing.Point(291, 290);
+            this.btn_Request.Location = new System.Drawing.Point(546, 278);
             this.btn_Request.Name = "btn_Request";
             this.btn_Request.Size = new System.Drawing.Size(252, 60);
             this.btn_Request.TabIndex = 11;
             this.btn_Request.Text = "Request Loan";
             this.btn_Request.UseVisualStyleBackColor = true;
             // 
+            // txt_AccountId
+            // 
+            this.txt_AccountId.Location = new System.Drawing.Point(646, 179);
+            this.txt_AccountId.Name = "txt_AccountId";
+            this.txt_AccountId.Size = new System.Drawing.Size(192, 22);
+            this.txt_AccountId.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(543, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Account ID :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.aCCOUNTIDDataGridViewTextBoxColumn,
+            this.cUSTOMERIDDataGridViewTextBoxColumn,
+            this.bALANCEDataGridViewTextBoxColumn,
+            this.tYPEDataGridViewTextBoxColumn,
+            this.iNTERESTRATEDataGridViewTextBoxColumn,
+            this.oPENDATEDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.aCCOUNTBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(856, 87);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(454, 261);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // bankSystemDataSet
+            // 
+            this.bankSystemDataSet.DataSetName = "BankSystemDataSet";
+            this.bankSystemDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // aCCOUNTBindingSource
+            // 
+            this.aCCOUNTBindingSource.DataMember = "ACCOUNT";
+            this.aCCOUNTBindingSource.DataSource = this.bankSystemDataSet;
+            // 
+            // aCCOUNTTableAdapter
+            // 
+            this.aCCOUNTTableAdapter.ClearBeforeFill = true;
+            // 
+            // aCCOUNTIDDataGridViewTextBoxColumn
+            // 
+            this.aCCOUNTIDDataGridViewTextBoxColumn.DataPropertyName = "ACCOUNTID";
+            this.aCCOUNTIDDataGridViewTextBoxColumn.HeaderText = "ACCOUNTID";
+            this.aCCOUNTIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.aCCOUNTIDDataGridViewTextBoxColumn.Name = "aCCOUNTIDDataGridViewTextBoxColumn";
+            this.aCCOUNTIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.aCCOUNTIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // cUSTOMERIDDataGridViewTextBoxColumn
+            // 
+            this.cUSTOMERIDDataGridViewTextBoxColumn.DataPropertyName = "CUSTOMERID";
+            this.cUSTOMERIDDataGridViewTextBoxColumn.HeaderText = "CUSTOMERID";
+            this.cUSTOMERIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.cUSTOMERIDDataGridViewTextBoxColumn.Name = "cUSTOMERIDDataGridViewTextBoxColumn";
+            this.cUSTOMERIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // bALANCEDataGridViewTextBoxColumn
+            // 
+            this.bALANCEDataGridViewTextBoxColumn.DataPropertyName = "BALANCE";
+            this.bALANCEDataGridViewTextBoxColumn.HeaderText = "BALANCE";
+            this.bALANCEDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.bALANCEDataGridViewTextBoxColumn.Name = "bALANCEDataGridViewTextBoxColumn";
+            this.bALANCEDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // tYPEDataGridViewTextBoxColumn
+            // 
+            this.tYPEDataGridViewTextBoxColumn.DataPropertyName = "TYPE";
+            this.tYPEDataGridViewTextBoxColumn.HeaderText = "TYPE";
+            this.tYPEDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tYPEDataGridViewTextBoxColumn.Name = "tYPEDataGridViewTextBoxColumn";
+            this.tYPEDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // iNTERESTRATEDataGridViewTextBoxColumn
+            // 
+            this.iNTERESTRATEDataGridViewTextBoxColumn.DataPropertyName = "INTERESTRATE";
+            this.iNTERESTRATEDataGridViewTextBoxColumn.HeaderText = "INTERESTRATE";
+            this.iNTERESTRATEDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.iNTERESTRATEDataGridViewTextBoxColumn.Name = "iNTERESTRATEDataGridViewTextBoxColumn";
+            this.iNTERESTRATEDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // oPENDATEDataGridViewTextBoxColumn
+            // 
+            this.oPENDATEDataGridViewTextBoxColumn.DataPropertyName = "OPENDATE";
+            this.oPENDATEDataGridViewTextBoxColumn.HeaderText = "OPENDATE";
+            this.oPENDATEDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.oPENDATEDataGridViewTextBoxColumn.Name = "oPENDATEDataGridViewTextBoxColumn";
+            this.oPENDATEDataGridViewTextBoxColumn.Width = 125;
+            // 
             // Request_Loan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(850, 395);
+            this.ClientSize = new System.Drawing.Size(1322, 395);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_AccountId);
             this.Controls.Add(this.btn_Request);
             this.Controls.Add(this.text_OriginationDate);
             this.Controls.Add(this.label5);
@@ -168,6 +286,9 @@
             this.Name = "Request_Loan";
             this.Text = "Request_Loan";
             this.Load += new System.EventHandler(this.Request_Loan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bankSystemDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aCCOUNTBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,5 +307,17 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox text_OriginationDate;
         private System.Windows.Forms.Button btn_Request;
+        private System.Windows.Forms.TextBox txt_AccountId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BankSystemDataSet bankSystemDataSet;
+        private System.Windows.Forms.BindingSource aCCOUNTBindingSource;
+        private BankSystemDataSetTableAdapters.ACCOUNTTableAdapter aCCOUNTTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aCCOUNTIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cUSTOMERIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bALANCEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tYPEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iNTERESTRATEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn oPENDATEDataGridViewTextBoxColumn;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Request Loan.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Request Loan.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApp1
 
         private void Request_Loan_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'bankSystemDataSet.ACCOUNT' table. You can move, or remove it, as needed.
+            this.aCCOUNTTableAdapter.Fill(this.bankSystemDataSet.ACCOUNT);
 
         }
     }
